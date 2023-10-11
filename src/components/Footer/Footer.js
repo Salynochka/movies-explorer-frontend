c87b9e73d12e84bcd55938914f9dd957ed0ac591
Alena Salynskaia <al_20.07@mail.ru>
2023-10-12 00:06:46 +0300

    Fixed footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,10 +6,11 @@ function Footer(props) {
       <h3 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h3>
       <div className="footer__info">
         <h4 className="footer__year">&copy; 2023</h4>
-        <div className="footer__nav">
+        <nav className="footer__nav">
           <a className="footer__link" href="https://practicum.yandex.ru/" target="_blank" rel="noreferrer">Яндекс.Практикум</a>
           <a className="footer__link" href="https://github.com/Salynochka" target="_blank" rel="noreferrer">Github</a>
-        </div>
+        </nav>
+        <h4 className="footer__year_mobile">&copy; 2023</h4>
       </div>
     </footer>
   );
