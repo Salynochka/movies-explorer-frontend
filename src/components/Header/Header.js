d7a90bb5d0355baff407cbf7ddce7eab4242151b
Alena Salynskaia <al_20.07@mail.ru>
2023-10-11 10:58:29 +0300

    Added profile page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,29 +1,30 @@
 import "./Header.css";
 import logo from "../../images/logo.svg";
 import { Link } from "react-router-dom";
+import account from "../../images/account.svg";
 
 function Header(props) {
   return (
-    <header className="header">
+    <header className={`${props.logIn ? "header-gray" : "header"}`}>
       <div className="header__info">
         <Link to="/">
           <img className="header__logo" src={logo} alt="Логотип сайта" />
         </Link>
         <div className={`header__navigation${props.logIn ? "_visible" : ""}`}>
-          <a className="header__films" href={props.films}>
-            Фильмы
-          </a>
-          <a className="header__saved-films" href={props.savedFilms}>
-            Сохраненные фильмы
-          </a>
+          <Link to="/" className="header__films">
+            <p>Фильмы</p>
+          </Link>
+          <Link to="/" className="header__saved-films">
+            <p>Сохраненные фильмы</p>
+          </Link>
         </div>
         <div className={`header__account${props.logIn ? "_visible" : ""}`}>
-          <p className="header__name">Аккаунт</p>
-          <img className="header__profile" src="photo" alt="Фото профиля" />
+          <Link to="/" className="header__name">
+            <p>Аккаунт</p>
+            <img className="header__profile" src={account} alt="Профиль" />
+          </Link>
         </div>
-        <div
-          className={`header__entrance-buttons${props.logIn ? "" : "_visible"}`}
-        >
+        <div className={`${props.logIn ? "" : "header__entrance-buttons"}`}>
           <Link to="/signup" className="header__register">
             Регистрация
           </Link>
